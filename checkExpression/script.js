475335049c4065c5cceb6eda67da4bf611ce1c94
Eduardo FM <eduardomarcos_@outlook.com>
2020-01-20 19:00:06 -0200

    implemented another solution using Maps

--- checkExpression/script.js
+++ checkExpression/script.js
@@ -83,3 +83,52 @@ if (total === 0) {
 } else sit = false;
 
 console.log(sit);
+
+/*******************************************/
+
+// Problema apresentado em uma entrevista de Software Engineer no Facebook: dado uma string com expressões abertas e fechadas por colchetes, parênteses e chaves,
+// retorne true se a formação estiver correta ou false do contrário.
+
+// Exemplo:
+// - [()] = true
+// - [(]) = false
+
+const array = ["[", "(", ")", "]", "(", ")"];
+//const array = ["(", ")"];
+const open = [];
+const check = new Map();
+
+const chaves = "{";
+const parenteses = "(";
+const colchetes = "[";
+
+check.set(chaves, "}");
+check.set(parenteses, ")");
+check.set(colchetes, "]");
+
+//console.log(check.get("("));
+let sit = true;
+function checkex() {
+  for (i = 0; i < array.length; i++) {
+    if (check.has(array[i])) {
+      //console.log("ok");
+      open.push(array[i]);
+    } else {
+      let lastOpened = open[open.length - 1];
+      console.log("lastopendd: ", lastOpened); //(
+      let openedMatch = check.get(lastOpened); //)
+      console.log("opened match: ", openedMatch);
+      console.log("el: ", array[i]);
+      if (array[i] !== openedMatch) {
+        return false;
+      } else {
+        open.pop();
+      }
+    }
+  }
+  if (open.length > 0) return false;
+  return true;
+}
+
+const result = checkex();
+console.log(result);
